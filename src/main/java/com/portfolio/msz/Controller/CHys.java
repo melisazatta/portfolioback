45547f7896a8dfb95c5e3978969cedd72a72b741
melisa <melisazatta@hotmail.com>
2023-05-16 01:55:52 -0300

    update url

--- src/main/java/com/portfolio/msz/Controller/CHys.java
+++ src/main/java/com/portfolio/msz/Controller/CHys.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = {"https://mszfrontend.firebaseapp.com", "http://localhost:4200"})
+@CrossOrigin(origins = {"https://mszfrontend.firebaseapp.com", "https://mszfrontend.web.app", "http://localhost:4200"})
 @RequestMapping("/skill")
 public class CHys {
 
--- src/main/java/com/portfolio/msz/Controller/PersonaController.java
+++ src/main/java/com/portfolio/msz/Controller/PersonaController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/personas")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"https://mszfrontend.firebaseapp.com", "https://mszfrontend.web.app", "http://localhost:4200"})
 public class PersonaController {
     @Autowired
     ImpPersonaService personaService;
