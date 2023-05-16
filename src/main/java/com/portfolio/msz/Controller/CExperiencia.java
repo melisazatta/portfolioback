79f646df0efced2d77a64852709dd743d497c750
melisa <melisazatta@hotmail.com>
2023-05-16 00:51:21 -0300

    update

--- src/main/java/com/portfolio/msz/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/msz/Controller/CExperiencia.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/explab")
-@CrossOrigin(origins = "http://localhost:4200") //{"https://mszfrontend.web.app","http://localhost:4200"})
+@CrossOrigin(origins = {"https://mszfrontend.firebaseapp.com", "http://localhost:4200"})
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
